e6d9821b00ae605b1d9a855b0d6b000f1f3b02ad
austin shields <ajshields196@gmail.com>
2021-06-10 21:49:11 -0400

    get data from backend and render basic datamap

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-// app.js
-const http = require('http');
+// // app.js
+// const http = require('http');
 
-// Create an instance of the http server to handle HTTP requests
-let app = http.createServer((req, res) => {
-  // Set a response type of plain text for the response
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+// // Create an instance of the http server to handle HTTP requests
+// let app = http.createServer((req, res) => {
+//   // Set a response type of plain text for the response
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  // Send back a response and end the connection
-  res.end('Hello World!\n');
-});
+//   // Send back a response and end the connection
+//   res.end('Hello World!\n');
+// });
 
-// Start the server on port 3000
-app.listen(8080, '127.0.0.1');
-console.log('Node server running on port 8080');
+// // Start the server on port 3000
+// app.listen(8080, '127.0.0.1');
+// console.log('Node server running on port 8080');
 
 
 
-// const axios = require("axios");
+const axios = require("axios");
+const Datamap = require("datamaps");
 
-// module.exports = function (n) {
-//   return n * 111;
-// };
+module.exports = function (n) {
+  return n * 111;
+};
 
-// document.getElementById("test").innerHTML = 5 + 6;
+var map = new Datamap({ element: document.getElementById('test') });
 
-// axios
-//   .get("http://localhost:3000/api/states")
-//   .then(response => {
-//     console.log("yay");
-//   })
-//   .catch(error => {
-//     console.log("aww");
-//   });
+axios
+  .get("http://localhost:3000/api/states")
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log("aww");
+  });
