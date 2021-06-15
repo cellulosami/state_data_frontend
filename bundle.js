205362f660ffc1b27a3fb31fb2ff58eb2d60ec6b
austin shields <ajshields196@gmail.com>
2021-06-14 22:15:48 -0400

    improve semantics

--- bundle.js
+++ bundle.js
@@ -1,50 +1,10 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-// // app.js
-// const http = require('http');
-
-// // Create an instance of the http server to handle HTTP requests
-// let app = http.createServer((req, res) => {
-//   // Set a response type of plain text for the response
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-//   // Send back a response and end the connection
-//   res.end('Hello World!\n');
-// });
-
-// // Start the server on port 3000
-// app.listen(8080, '127.0.0.1');
-// console.log('Node server running on port 8080');
-
-
-
 const axios = require("axios");
 const Datamap = require("datamaps");
 
 module.exports = function (n) {
   return n * 111;
 };
-
-function renderMap(stateData) {
-  var election = new Datamap({
-    scope: 'usa',
-    element: document.getElementById('test'),
-    geographyConfig: {
-      highlightBorderColor: '#209ba4',
-      highlightFillColor: '#33cdd8',
-      popupTemplate: function (geography, data) {
-        return `<div class="hoverinfo"> <strong>` + geography.properties.name + `</strong><br/> 
-        Median household income: $` + data.median_household_income + ` <br/>
-        Share of population in metro areas: ` + (data.share_population_in_metro_areas * 100).toFixed(1) + `% <br/>
-        Share of population with a high school degree: ` + (data.share_population_with_high_school_degree * 100).toFixed(1) + `% <br/>
-        Share of population unemployed seasonally: ` + (data.share_unemployed_seasonal * 100).toFixed(1) + `% `;
-      },
-      highlightBorderWidth: 3
-    },
-    data: stateData
-  });
-  election.labels();
-}
-
 
 axios
   .get("http://localhost:3000/api/states/frontend")
@@ -56,6 +16,32 @@ axios
     console.log(error);
     console.log(error.response.statusText);
   });
+
+function renderMap(stateData) {
+  var election = new Datamap({
+    scope: 'usa',
+    element: document.getElementById('test'),
+    geographyConfig: {
+      highlightBorderColor: '#209ba4',
+      highlightFillColor: '#33cdd8',
+      popupTemplate: function (geography, data) {
+        return `<div class="hoverinfo"> <strong>` + geography.properties.name + `</strong><br/> 
+
+        Median household income: $` + data.median_household_income + ` <br/>
+
+        Share of population in metro areas: ` + (data.share_population_in_metro_areas * 100).toFixed(1) + `% <br/>
+
+        Share of population with a high school degree: ` + (data.share_population_with_high_school_degree * 100).toFixed(1) + `% <br/>
+
+        Share of population unemployed seasonally: ` + (data.share_unemployed_seasonal * 100).toFixed(1) + `% `;
+      },
+      highlightBorderWidth: 3
+    },
+    data: stateData
+  });
+  election.labels();
+}
+
 },{"axios":2,"datamaps":30}],2:[function(require,module,exports){
 module.exports = require('./lib/axios');
 },{"./lib/axios":4}],3:[function(require,module,exports){
--- main.js
+++ main.js
@@ -1,49 +1,9 @@
-// // app.js
-// const http = require('http');
-
-// // Create an instance of the http server to handle HTTP requests
-// let app = http.createServer((req, res) => {
-//   // Set a response type of plain text for the response
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-//   // Send back a response and end the connection
-//   res.end('Hello World!\n');
-// });
-
-// // Start the server on port 3000
-// app.listen(8080, '127.0.0.1');
-// console.log('Node server running on port 8080');
-
-
-
 const axios = require("axios");
 const Datamap = require("datamaps");
 
 module.exports = function (n) {
   return n * 111;
 };
-
-function renderMap(stateData) {
-  var election = new Datamap({
-    scope: 'usa',
-    element: document.getElementById('test'),
-    geographyConfig: {
-      highlightBorderColor: '#209ba4',
-      highlightFillColor: '#33cdd8',
-      popupTemplate: function (geography, data) {
-        return `<div class="hoverinfo"> <strong>` + geography.properties.name + `</strong><br/> 
-        Median household income: $` + data.median_household_income + ` <br/>
-        Share of population in metro areas: ` + (data.share_population_in_metro_areas * 100).toFixed(1) + `% <br/>
-        Share of population with a high school degree: ` + (data.share_population_with_high_school_degree * 100).toFixed(1) + `% <br/>
-        Share of population unemployed seasonally: ` + (data.share_unemployed_seasonal * 100).toFixed(1) + `% `;
-      },
-      highlightBorderWidth: 3
-    },
-    data: stateData
-  });
-  election.labels();
-}
-
 
 axios
   .get("http://localhost:3000/api/states/frontend")
@@ -55,3 +15,28 @@ axios
     console.log(error);
     console.log(error.response.statusText);
   });
+
+function renderMap(stateData) {
+  var election = new Datamap({
+    scope: 'usa',
+    element: document.getElementById('test'),
+    geographyConfig: {
+      highlightBorderColor: '#209ba4',
+      highlightFillColor: '#33cdd8',
+      popupTemplate: function (geography, data) {
+        return `<div class="hoverinfo"> <strong>` + geography.properties.name + `</strong><br/> 
+
+        Median household income: $` + data.median_household_income + ` <br/>
+
+        Share of population in metro areas: ` + (data.share_population_in_metro_areas * 100).toFixed(1) + `% <br/>
+
+        Share of population with a high school degree: ` + (data.share_population_with_high_school_degree * 100).toFixed(1) + `% <br/>
+
+        Share of population unemployed seasonally: ` + (data.share_unemployed_seasonal * 100).toFixed(1) + `% `;
+      },
+      highlightBorderWidth: 3
+    },
+    data: stateData
+  });
+  election.labels();
+}
